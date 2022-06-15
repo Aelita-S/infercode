5b7fca40f137ed4cd8d6be92e90d4054ba6259ba
Aelita-S <1225438527@qq.com>
2022-06-16 02:39:11 +0800

    test: update test_infer.py

--- infercode/tests/test_infer.py
+++ infercode/tests/test_infer.py
@@ -2,13 +2,15 @@ import logging
 import os
 
 from infercode.client.infercode_client import InferCodeClient
+from infercode.settings import SAVED_MODEL_PATH
 
 logging.basicConfig(level=logging.INFO)
 
-# Change from -1 to 0 to enable GPU
-os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
+if __name__ == '__main__':
+    # Change from -1 to 0 to enable GPU
+    os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
 
-infercode = InferCodeClient()
-vectors = infercode.encode(["for (i = 0; i < n; i++)", "struct book{ int num; char s[27]; }shu[1000];"])
+    infercode = InferCodeClient(model_path=SAVED_MODEL_PATH)
+    vectors = infercode.encode(["for (i = 0; i < n; i++)", "struct book{ int num; char s[27]; }shu[1000];"])
 
-print(vectors)
+    print(vectors)
